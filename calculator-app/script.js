c247766c935e971fe122ff363ed2e9b73294158d
kishan-kumar-sahu <kumarkishan26636@gmail.com>
2025-04-24 10:48:03 -0700

    Calculator-App-Javascript

---
+++ calculator-app/script.js
@@ -0,0 +1,43 @@
+    // evel ek method hota  hai jo string  ko  evalute karta hai 
+
+// const expre= "2+3*(4-1)";
+ 
+// const result= eval(expre)
+
+// console.log(result)  // 11
+
+
+
+  /*
+ // some example
+
+    '12'+'13'=>'1213'
+        '12'+'*'=>'12*'
+    eval('12'+'13')=> 1213
+    eval('12+13')=> 25
+
+    '12'-'13'=> '-1'
+
+  */
+
+
+
+
+
+
+
+
+   //  build  calculator  project
+
+
+    let currentvalue=""
+   const display= document.querySelector("#displalid");
+   console.dir(display)
+       display.value= currentvalue
+
+
+
+
+
+
+
